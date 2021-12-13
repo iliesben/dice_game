abd81feac78efd9531b3dcecece905e3973b5c06
j <mahamadoun.toure1@gmail.com>
2021-12-13 22:21:16 +0100

    added player setup function

--- dice.py
+++ dice.py
@@ -42,34 +42,34 @@ def roll_dice_set(nb_dice_to_roll):
     for n in range(nb_dice_to_roll):
         dice_value = random.randint(1, NB_DICE_SIDE)
         dice_value_occurrence_list[dice_value - 1] += 1
-
     return dice_value_occurrence_list
 
 
+# return a bonus score and the new dice value occurrence without the bonus elements
 def analyse_bonus_score(dice_value_occurrence_list):
     score = 0
     for side_value_index, dice_value_occurrence in enumerate(dice_value_occurrence_list):
         nb_of_bonus = dice_value_occurrence // THRESHOLD_BONUS
         if nb_of_bonus > 0:
             if side_value_index == 0:
-                bonus_multiplier = ACE_BONUS_MULTIPLIER
+                bonus_multiplier = BONUS_VALUE_FOR_ACE_BONUS
             else:
-                bonus_multiplier = STD_BONUS_MULTIPLIER
+                bonus_multiplier = BONUS_VALUE_FOR_NORMAL_BONUS
             score += nb_of_bonus * bonus_multiplier * (side_value_index + 1)
             dice_value_occurrence_list[side_value_index] %= THRESHOLD_BONUS
-
     return score, dice_value_occurrence_list
 
 
+# return a standard score and dice value occurence
 def analyse_standard_score(dice_value_occurrence_list):
     score = 0
-    for scoring_value, scoring_multiplier in zip(SCORING_DICE_VALUE_LIST, SCORING_MULTIPLIER_LIST):
+    for scoring_value, scoring_multiplier in zip(LIST_SCORING_DICE_VALUE, LIST_SCORING_MULTIPLIER):
         score += dice_value_occurrence_list[scoring_value - 1] * scoring_multiplier
         dice_value_occurrence_list[scoring_value - 1] = 0
-
     return score, dice_value_occurrence_list
 
 
+# returns the global score of the dice set and dice value occurence
 def analyse_score(dice_value_occurrence_list):
     bonus_score, dice_value_occurrence_list = analyse_bonus_score(dice_value_occurrence_list)
     standard_score, dice_value_occurrence_list = analyse_standard_score(dice_value_occurrence_list)
@@ -77,9 +77,16 @@ def analyse_score(dice_value_occurrence_list):
     return bonus_score + standard_score, dice_value_occurrence_list
 
 
-# print(analyse_score([7, 2, 3, 0, 4, 1]))
+# returns the list of players
+def set_player():
+    players = int(input("Enter number of players:""\n", ))
+    player_list = []
+    for n in range(players):
+        name = input("Enter your name:""\n", )
+        player_list.append(name)
+    return player_list
 
-players = []
+
 
 def setPlayer(player):
   Numberplayer = int(input('Enter number player: '))
