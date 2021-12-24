e0b5f08b4935d0303b11b409dad37f50a36df3f4
j <mahamadoun.toure1@gmail.com>
2021-12-24 14:43:56 +0100

    add player class model

---
+++ DiceGame_Web/models/player.py
@@ -0,0 +1,66 @@
+class Player:
+
+    def __init__(self, name):
+        self.name = name
+        self.score = 0
+        self.bonus_count = 0
+        self.lost_count = 0
+        self.roll_count = 0
+        self._full_roll_count = 0
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @property
+    def score(self) -> str:
+        return self.score
+
+    @name.setter
+    def score(self, value):
+        self.score = value
+
+    @property
+    def bonus_count(self) -> int:
+        return self._score
+
+    @bonus_count.setter
+    def bonus_count(self, value):
+        self._bonus_count = value
+
+    @property
+    def lost_points(self) -> int:
+        return self._lost_count
+
+    @lost_points.setter
+    def lost_points(self, value):
+        self._lost_points = value
+
+    @property
+    def roll_count(self) -> int:
+        return self._roll_count
+
+    @roll_count.setter
+    def roll_count(self, value):
+        self._roll_count = value
+
+    @property
+    def full_roll_count(self) -> int:
+        return self._full_roll_count
+
+    @full_roll_count.setter
+    def full_roll_count(self, value):
+        self._full_roll_count = value
+
+
+def add_player():
+    players = int(input("Enter number of players:""\n", ))
+    player_list = []
+    for _ in range(players):
+        name = input("Enter your name:""\n", )
+        player_list.append(Player(name))
+    return player_list
