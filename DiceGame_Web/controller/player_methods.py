d40ae3897e7af6be2b09cd8d5e051d6d6ae3f84d
j <mahamadoun.toure1@gmail.com>
2022-01-03 12:50:06 +0100

    reduce the cognitive complexity

--- DiceGame_Web/controller/player_methods.py
+++ DiceGame_Web/controller/player_methods.py
@@ -1,4 +1,4 @@
-from random import random
+
 import utils.constants as params
 from models.player import Player
 import controller.score_methods as score
@@ -15,34 +15,32 @@ def launch_dice(current_player):
     scoring = 0
     dice_remaining = params.GAME_MAX_ROLL
     nbr_scoring = 0
-    while True:
-        while dice_remaining > 0:
-            response = input(Player.get_name(current_player) + " tap [y] or [yes] for roll dice! ").lower()
-            if response in ['y', 'yes']:
-                dice_value_occurrence_list = dice.roll_dice_set()
-                scoring_dice = dice.scoring_dices(dice_value_occurrence_list)
-                nbr_scoring = dice.nbr_scoring_dice(dice_value_occurrence_list)
-                scoring = score.analyse_score(dice_value_occurrence_list, current_player)
-                potential_score = potential_score + scoring
-                dice_remaining = dice_remaining - nbr_scoring
-                if dice_remaining<0:
-                    dice_remaining = 0
-                print(
-                    "Roll #" + str(roll) + " : " + str(nbr_scoring)
-                    + " scoring dices " + str(scoring_dice)
-                    + " scoring " + str(scoring)
-                    + ", potential total turn score " + str(potential_score)
-                    + ", remaining dice to roll : " + str(dice_remaining)
-                )
-                roll = roll + 1
-            else:
-                print("You win this turn, your score " + str(potential_score) + " pts")
-                return scoring, potential_score
-            if dice_remaining < 1:
-                return scoring, potential_score
-            if nbr_scoring == 0:
-                # print(display_scoring)
-                print("You lose this turn and a potential to score " + str(potential_score) + " pts")
-                scoring = 0
-                return scoring, potential_score
-        return scoring, potential_score
+    while dice_remaining > 0:
+        response = input(Player.get_name(current_player) + " tap [y] or [yes] for roll dice! ").lower()
+        if response in ['y', 'yes']:
+            dice_value_occurrence_list = dice.roll_dice_set()
+            scoring_dice = dice.scoring_dices(dice_value_occurrence_list)
+            nbr_scoring = dice.nbr_scoring_dice(dice_value_occurrence_list)
+            scoring = score.analyse_score(dice_value_occurrence_list, current_player)
+            potential_score = potential_score + scoring
+            dice_remaining = dice_remaining - nbr_scoring
+            if dice_remaining < 0:
+                dice_remaining = 0
+            print(
+                "Roll #" + str(roll) + " : " + str(nbr_scoring)
+                + " scoring dices " + str(scoring_dice)
+                + " scoring " + str(scoring)
+                + ", potential total turn score " + str(potential_score)
+                + ", remaining dice to roll : " + str(dice_remaining)
+            )
+            roll = roll + 1
+        else:
+            print("You win this turn, your score " + str(potential_score) + " pts")
+            return scoring, potential_score
+        if dice_remaining < 1:
+            return scoring, potential_score
+        if nbr_scoring == 0:
+            # print(display_scoring)
+            print("You lose this turn and a potential to score " + str(potential_score) + " pts")
+            scoring = 0
+            return scoring, potential_score
