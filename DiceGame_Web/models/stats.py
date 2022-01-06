32d0a0d7b0f255d676e85f66401fbc2151165093
j <mahamadoun.toure1@gmail.com>
2022-01-06 22:17:46 +0100

    Add stats class

---
+++ DiceGame_Web/models/stats.py
@@ -0,0 +1,10 @@
+
+class Stats:
+    def __init__(self):
+        self.max_score = 0
+        self.longest_turn = 0
+        self.max_turn_loss = 0 # max score turn
+        self.mean_scoring_turn = 0
+        self.mean_non_scoring_turn = 0
+        self.all_score = 0
+        self.all_non_score = 0
